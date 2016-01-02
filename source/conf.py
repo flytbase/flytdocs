481e1ec996e9a8e7e7e52904979dcfa565a36d58
Sharvashish Das <sharvashish@navstik.org>
2016-01-02 22:06:25 +0530

    cjhsce

--- source/conf.py
+++ source/conf.py
@@ -20,6 +20,8 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:  # only import and set the theme if we're building docs locally
   html_context = { 
     'css_files': [
+        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
         '_static/custom.css',
     ],  
   }  
