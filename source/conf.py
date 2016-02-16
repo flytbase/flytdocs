912b26d8d89ca4a7f5db90ab91d931725d97ca72
Sharvashish Das <sharvashish@navstik.org>
2016-02-16 22:47:14 +0530

    updated FlytAPIs and RouterSetup

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ sys.path.append(os.path.abspath('_extensions/sphinxcontrib'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc','youtube'
+    'sphinx.ext.autodoc','youtube12'
 ]
 
 # images_config = dict(
