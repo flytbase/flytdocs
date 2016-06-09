450a7a3a7e2c3a3666f288f07334d548460f278a
Sharvashish Das <sharvashish@navstik.org>
2016-06-09 18:32:47 +0530

    improved dependency list

--- source/docs/FlytOS/include/flytos_dependency.sh
+++ source/docs/FlytOS/include/flytos_dependency.sh
@@ -3,10 +3,10 @@
 # TODO : for future, download md5 file and check md5sum
 
 #installing known ros dependencies
-apt-get install ros-indigo-usb-cam ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-pyros-setup ros-indigo-eigen-conversions
+apt-get install -y --force-yes ros-indigo-usb-cam ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-pyros-setup ros-indigo-eigen-conversions
 #installing known python dependencies
-apt-get install python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
-apt-get install python-pip
+apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
+apt-get install -y --force-yes python-pip
 pip install flask-security
 
 exit 0
--- source/docs/FlytSim/include/flytsim_install.sh
+++ source/docs/FlytSim/include/flytsim_install.sh
@@ -3,12 +3,12 @@
 # TODO : for future, download md5 file and check md5sum
 
 #confirming gazebo6 and its ros dependencies are installed
-apt-get install gazebo6 libgazebo6-dev ros-indigo-gazebo6-*
+apt-get install -y --force-yes gazebo6 libgazebo6-dev ros-indigo-gazebo6-*
 #installing known ros dependencies
-apt-get install ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-octomap-ros ros-indigo-octomap-msgs libgoogle-glog-dev ros-indigo-pyros-setup ros-indigo-eigen-conversions
+apt-get install -y --force-yes ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-octomap-ros ros-indigo-octomap-msgs libgoogle-glog-dev ros-indigo-pyros-setup ros-indigo-eigen-conversions
 #installing known python dependencies
-apt-get install python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
-apt-get install python-pip
+apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
+apt-get install -y --force-yes python-pip
 pip install flask-security
 #installing FlytSim package
 dpkg -i flytsim_0.4-1_amd64.deb  
