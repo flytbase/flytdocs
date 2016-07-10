e30ab0728356492712ba2140677093328c4abe2d
Sharvashish Das <sharvashish@navstik.org>
2016-07-10 17:17:54 +0530

    updated navbar font to oswald

--- source/docs/FlytOS/include/flytos_dependency.sh
+++ source/docs/FlytOS/include/flytos_dependency.sh
@@ -5,7 +5,7 @@
 #installing known ros dependencies
 apt-get install -y --force-yes ros-indigo-usb-cam ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-pyros-setup ros-indigo-eigen-conversions
 #installing known python dependencies
-apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
+apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq python-twisted
 apt-get install -y --force-yes python-pip
 pip install flask-security
 
--- source/docs/FlytSim/include/flytsim_install.sh
+++ source/docs/FlytSim/include/flytsim_install.sh
@@ -7,7 +7,7 @@ apt-get install -y --force-yes gazebo6 libgazebo6-dev ros-indigo-gazebo6-*
 #installing known ros dependencies
 apt-get install -y --force-yes ros-indigo-image-proc ros-indigo-image-transport-plugins ros-indigo-image-transport ros-indigo-rosbridge-suite ros-indigo-control-toolbox ros-indigo-octomap-ros ros-indigo-octomap-msgs libgoogle-glog-dev ros-indigo-pyros-setup ros-indigo-eigen-conversions
 #installing known python dependencies
-apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq 
+apt-get install -y --force-yes python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq python-twisted
 apt-get install -y --force-yes python-pip
 pip install flask-security
 #installing FlytSim package
