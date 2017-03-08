f5a3d760055c31a969a5b06705162534cc80bd77
Zubin Priyansh <zubin94sharma@gmail.com>
2017-03-08 10:45:53 +0530

    Added requests to the list of dependencies

--- source/docs/FlytOS/include/flytos_dependency.sh
+++ source/docs/FlytOS/include/flytos_dependency.sh
@@ -3,7 +3,7 @@
 
 #installing known python dependencies
 sudo apt install -y python-pip python-serial python-flask python-wtforms python-sqlalchemy python-concurrent.futures python-mock python-zmq python-twisted python-future
-sudo -H pip install flask_cors flask_reverse_proxy flask_restful tblib webargs click flask_security httplib2 simple_json pyzmp pyros-setup
+sudo -H pip install flask_cors flask_reverse_proxy flask_restful tblib webargs click flask_security httplib2 simple_json pyzmp pyros-setup requests
 
 #installing known ros dependencies
 sudo apt install -y ros-kinetic-image-proc ros-kinetic-image-transport-plugins ros-kinetic-image-transport ros-kinetic-rosbridge-suite ros-kinetic-control-toolbox ros-kinetic-eigen-conversions ros-kinetic-camera-info-manager ros-kinetic-pyros-utils libxslt-dev libxml2-dev 
